ab13e3a7a59dc4826e1e528a96b5c764b6b056c4
VIBHA <artvibha2007@gmail.com>
2026-01-20 20:30:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,28 @@
-let foods = [];
+function showDonor() {
+  document.getElementById("donorSection").style.display = "block";
+  document.getElementById("ngoSection").style.display = "none";
+}
+
+function showNGO() {
+  document.getElementById("donorSection").style.display = "none";
+  document.getElementById("ngoSection").style.display = "block";
+}
 
 function postFood() {
-  const donor = document.getElementById("donorName").value;
-  const food = document.getElementById("foodName").value;
+  const name = document.getElementById("donorName").value;
+  const food = document.getElementById("foodDetails").value;
   const location = document.getElementById("location").value;
 
-  if (!donor || !food || !location) {
-    alert("Fill all fields");
+  if (!name || !food || !location) {
+    alert("Please fill all fields");
     return;
   }
 
-  foods.push({
-    donor,
-    food,
-    location,
-    claimed: false
-  });
+  const li = document.createElement("li");
+  li.innerHTML = `${food} | ${location} | by ${name} 
+    <button onclick="this.parentElement.remove()">Claim</button>`;
 
-  displayFood();
+  document.getElementById("foodList").appendChild(li);
 
-  document.getElementById("donorName").value = "";
-  document.getElementById("foodName").value = "";
-  document.getElementById("location").value = "";
-}
-
-function displayFood() {
-  const foodList = document.getElementById("foodList");
-  foodList.innerHTML = "";
-
-  foods.forEach((item, index) => {
-    const li = document.createElement("li");
-
-    li.innerHTML = `
-      <b>${item.food}</b> | ${item.location} | by ${item.donor}
-      <br>
-      ${
-        item.claimed
-          ? "<span style='color:green;'>✔ Claimed</span>"
-          : `<button onclick="claimFood(${index})">Claim Food</button>`
-      }
-    `;
-
-    foodList.appendChild(li);
-  });
-}
-
-function claimFood(index) {
-  foods[index].claimed = true;
-  displayFood();
+  alert("Food posted successfully!");
 }
